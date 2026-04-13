17a3aa5b3adf97330fb74cf45270c62b7c0c186c
chosenol <lilyoung2411@gmail.com>
2026-04-13 14:15:14 +0700

    fix

--- sieve.py
+++ sieve.py
@@ -1,12 +1,12 @@
 # # # # check so ngto 
-# # # # def sieve(n):
-# # # #     is_prime = [True] * (n + 1)
-# # # #     is_prime[0] = is_prime[1] = False
-# # # #     for i in range(2, int(n**0.5) + 1):
-# # # #         if is_prime[i]:
-# # # #             for j in range(i*i, n + 1, i):
-# # # #                 is_prime[j] = False
-# # # #     return is_prime
+def sieve(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
 
 # # # # t = int(input())
 # # # # arr = [int(input()) for _ in range(t)]
@@ -18,38 +18,38 @@
 # # # # ktra so nguyen to va chu so cũng là số nguyên tố
 # # # # viết chương trình đếm xem trong đoạn a,b  có bao nhiêu số là snt và tất cả các chữ số đều là số nguyên tố
 
-# # # def sang(n):
-# # #     is_prime = [True] * (n+1)
-# # #     is_prime[0] = is_prime[1] = False
-# # #     for i in range(2, int(n ** 0.5) + 1):
-# # #         if is_prime[i]:
-# # #             for j in range(i*i, n + 1, i):
-# # #                 is_prime[j] = False
-# # #     return is_prime
-# # #     # return [i for i in range(2, n + 1) if is_prime[i]]
+def sang(n):
+    is_prime = [True] * (n+1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
+    # return [i for i in range(2, n + 1) if is_prime[i]]
 
-# # # # def segment_sieve(L, R):
-# # # #     Limit = int(R ** 0.5) + 1
-# # # #     primes = sang(Limit)
-# # # #     is_prime = [True] * (R - L + 1)
-# # # #     for p in primes:
-# # # #         start = max(p*p, (( L + p - 1)//p)*p)
-# # # #         for j in range(start, R + 1, p):
-# # # #             is_prime[j - L] = False
+def segment_sieve(L, R):
+    Limit = int(R ** 0.5) + 1
+    primes = sang(Limit)
+    is_prime = [True] * (R - L + 1)
+    for p in primes:
+        start = max(p*p, (( L + p - 1)//p)*p)
+        for j in range(start, R + 1, p):
+            is_prime[j - L] = False
 
-# # # #     for i in range(len(is_prime)):
-# # # #         if is_prime[i] and (i + L) >= 2:
-# # # #             print(i + L, end=" ")
+    for i in range(len(is_prime)):
+        if is_prime[i] and (i + L) >= 2:
+            print(i + L, end=" ")
 
-# # # # def digitPrime(n):
-# # # #     while(n):
-# # # #         r = n % 10
-# # # #         if r not in [2,3,5,7]:
-# # # #             return False
-# # # #         n //= 10
-# # # #     return True
+def digitPrime(n):
+    while(n):
+        r = n % 10
+        if r not in [2,3,5,7]:
+            return False
+        n //= 10
+    return True
 
-# # # MAX_N = 10**6
+MAX_N = 10**6
 # # # # # is_prime = sang(MAX_N)
 
 # # # # t = int(input())
@@ -63,23 +63,23 @@
 
 # # # ---------------------- ĐÉM TRONG ĐOẠN ĐÓ CÓ BAO NHIÊU SỐ NGUYÊN TỐ-----------------------
 
-# # # def ssnt(n):
-# # #     is_prime = [True]*(n+1)
-# # #     is_prime[0] = is_prime[1] = False
-# # #     for i in range(2, int(n**0.5)):
-# # #         if is_prime[i]:
-# # #             for j in range(i*i, n + 1, i ):
-# # #                 is_prime[j] = False
-# # #     return is_prime
+def ssnt(n):
+    is_prime = [True]*(n+1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n**0.5)):
+        if is_prime[i]:
+            for j in range(i*i, n + 1, i ):
+                is_prime[j] = False
+    return is_prime
 
-# # # t = int(input())
-# # # cnt = i = 0
-# # # is_prime = ssnt(MAX_N)
-# # # while cnt < t:
-# # #     if is_prime[i]:
-# # #         cnt += 1
-# # #         print(i)
-# # #     i += 1
+t = int(input())
+cnt = i = 0
+is_prime = ssnt(MAX_N)
+while cnt < t:
+    if is_prime[i]:
+        cnt += 1
+        print(i)
+    i += 1
 
 # # # --------------------- CẶP SỐ NGUYÊN TỐ CÓ TỔNG BẰNG N---------------------------
 # # # def sang(n):
@@ -94,12 +94,12 @@
 # # # MAX_N = 10**6
 # # # is_prime = sang(MAX_N)
 
-# # # t = int(input())
-# # # for _ in range(t):
-# # #     n = int(input())
-# # #     for i in range (2, int(n/2 + 1)):
-# # #         if is_prime[i] and is_prime[n-i]:
-# # #             print(i, n-i)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    for i in range (2, int(n//2 + 1)):
+        if is_prime[i] and is_prime[n-i]:
+            print(i, n-i)
 
          
 # # # ---------------------SỐ THUẦN NGUYÊN TỐ-----------------
